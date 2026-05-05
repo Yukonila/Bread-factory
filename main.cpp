69988d78933c9ddc2d52030d8864ed0f4629d6c0
Koni <1968550980@qq.com>
2026-05-05 16:02:26 +0800

    update project files

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@ using namespace std;
 
 int main() {
     string i = "Arch";
-    int sum = 0;
     cout << "Hello and welcome to " << i << "!\n";
-    for (int a = 0; a < 100; ++a) {
-        sum += a;
-    }
-
-
     return 0;
 }
